e46ec549704ce1484bb2d39e411d70c1f8ecc11f
Madni-investment-scheme <testing34kk@gmail.com>
2026-09-06 02:26:25 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 3000;
 
 // Services data with description
 const services = [
-    { id: 1, name: 'TikTok Likes (1000)', price: 120, desc: 'Best Likes Start in 0-5 minutes -- Fast Speed -- Complete In 1-3 Hours -- Guarenty Puri Zindagi Ki ♻️' },
-    { id: 2, name: 'Instagram Followers (1000)', price: 250, desc: 'High Quality Real Followers -- Non-Drop -- Start Time: 0-1 Hour.' },
+    { id: 1, name: 'TikTok Likes (1000)', price: 120, min: 100, max: 10000, desc: 'Best Likes Start in 0-5 minutes -- Fast Speed -- Complete In 1-3 Hours -- Guarenty Puri Zindagi Ki ♻️' },
+{ id: 1, name: 'Tiktok Views (1000)', price: 40, min: 500, max: 1000, desc: 'Best Views Start in 0-5 minutes -- Fast Speed -- Complete In 1-3 Hours -- Guarenty Puri Zindagi Ki ♻️' },
     { id: 3, name: 'TikTok Followers (1000)', price: 300, desc: 'Active Profiles -- Instant Start -- Safe & Secure.' }
 ];
 
