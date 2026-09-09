37810ad851e1dcaa59592d1d6bf5c28940c56cc5
Madni-investment-scheme <testing34kk@gmail.com>
2026-09-10 01:43:56 +0500

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const services = [
     { id: 9, name: 'Facebook Views Lifetime Guarantee (1000)', price: 70, min: 100, max: 10000000, desc: 'Best Facebook Views Video/Reel All Link|Max Unlimited| -- Fast Speed -- Complete In 1-3 hours -- Guarenty Puri Zindagi ki hai ager kam ho jayen to dubara complete karwa sakte ho-' },
     { id: 10, name: 'Facebook Likes 30days Guarantee (1000)', price: 100, min: 20, max: 500000, desc: 'Best Facebook Likes(High-quality Account) |200k/day| -- Fast Speed -- Complete with 30 minutes hours -- Guarenty 30 days ki hai ager 30 days se pehle kam ho jayen to dubara complete karwa sakte ho-' },
     { id: 11, name: 'Facebook Random Comments Pakistani (1000)', price: 1250, min: 10, max: 100000, desc: 'Best Facebook Random Comments (High-quality Account) |1k/day| -- Instant Start -- Complete In 1hour 30 minutes-3hours --Guarenty Bilkul Bhi Ni Khatam Ya Kam Bhi Ho Sakte Hain-' },
-    { id: 12, name: 'Facebook Custom Comments Pakistani (1000)', price: 1350, min: 10, max: 100000, desc: 'Best Facebook Custom Comments (High-quality Account) |1k/day| -- Instant Start -- Complete In 2hour 50 minutes-5hours --Guarenty Bilkul Bhi Ni Khatam Ya Kam Bhi Ho Sakte Hain-' },
+    { id: 12, name: 'Facebook Custom Comments Pakistani (1000)', price: 1350, min: 10, max: 100000, desc: 'Best Facebook Custom Comments Or Yaad Rahe k comments whatsapp pr send kardena(High-quality Account) |1k/day| -- Instant Start -- Complete In 2hour 50 minutes-5hours --Guarenty Bilkul Bhi Ni Khatam Ya Kam Bhi Ho Sakte Hain-' },
     
 ];
 
